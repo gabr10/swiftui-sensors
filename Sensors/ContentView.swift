f6826d360cb154ab7a29c9db614e8ff50bfe8c1f
Gabrio Barbieri <gbarbier@akamai.com>
2023-05-11 09:37:24 -0600

    v1 all 6 sensors up

--- Sensors/ContentView.swift
+++ Sensors/ContentView.swift
@@ -6,17 +6,166 @@
 //
 
 import SwiftUI
+import Combine
+import Charts
 
+enum Constants {
+    static let lineAmount = 40
+    static let magLimit: Float = 3
+    static let updateInterval = 0.03
+}
 struct ContentView: View {
+    
+    
+    let timer = Timer.publish(
+            every: Constants.updateInterval,
+            on: .main,
+            in: .common
+        ).autoconnect()
+    
+    @ObservedObject var accelerometerManager = AccelerometerManager()
+    @State var dataDict: SensorData = SensorData(x: [], y: [], z: [], rx: [], ry: [], rz: [])
+    @State var x:[Float] = []
+    @State var y:[Float] = []
+    @State var z:[Float] = []
+    @State var rx:[Float] = []
+    @State var ry:[Float] = []
+    @State var rz:[Float] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 16) {
+            Text("Accelerometer Data")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.top, 40)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Group {
+                    Chart(Array(x.enumerated()), id:\.0){ index, magnitud in
+                        LineMark(x: .value("Frequency", String(index)), y: .value("Magnitude", magnitud))
+                            .foregroundStyle(.secondary)
+                        
+                    }
+                    .onReceive(timer, perform: updateData)
+                    .chartYScale(domain: -3 ... Constants.magLimit)
+                    .chartXAxis(.hidden)
+                    .background(Color.secondary.opacity(0.2))
+                    .clipped()
+                    .cornerRadius(10)
+                    
+                    Spacer()
+                }
+                Group {
+                    Chart(Array(y.enumerated()), id:\.0){ index, magnitud in
+                        LineMark(x: .value("Frequency", String(index)), y: .value("Magnitude", magnitud))
+                        
+                    }
+                    .onReceive(timer, perform: updateData)
+                    .chartYScale(domain: -3 ... Constants.magLimit)
+                    .chartXAxis(.hidden)
+                    .background(Color.secondary.opacity(0.2))
+                    .cornerRadius(10)
+                    Spacer()
+                }
+                Group {
+                    Chart(Array(z.enumerated()), id:\.0){ index, magnitud in
+                        LineMark(x: .value("Frequency", String(index)), y: .value("Magnitude", magnitud))
+                        
+                    }
+                    .onReceive(timer, perform: updateData)
+                    .chartYScale(domain: -3 ... Constants.magLimit)
+                    .chartXAxis(.hidden)
+                    .background(Color.secondary.opacity(0.2))
+                    .cornerRadius(10)
+                    Spacer()
+                }
+                Group {
+                    Chart(Array(rx.enumerated()), id:\.0){ index, magnitud in
+                        LineMark(x: .value("Frequency", String(index)), y: .value("Magnitude", magnitud))
+                        
+                    }
+                    .onReceive(timer, perform: updateData)
+                    .chartYScale(domain: -3 ... Constants.magLimit)
+                    .chartXAxis(.hidden)
+
+                    .background(Color.secondary.opacity(0.2))
+                    .cornerRadius(10)
+                    Spacer()
+                }
+                Group {
+                    Chart(Array(ry.enumerated()), id:\.0){ index, magnitud in
+                        LineMark(x: .value("Frequency", String(index)), y: .value("Magnitude", magnitud))
+                        
+                    }
+                    .onReceive(timer, perform: updateData)
+                    .chartYScale(domain: -3 ... Constants.magLimit)
+                    .chartXAxis(.hidden)
+                    .background(Color.secondary.opacity(0.2))
+                    .cornerRadius(10)
+                    Spacer()
+                }
+                Group {
+                    Chart(Array(rz.enumerated()), id:\.0){ index, magnitud in
+                        LineMark(x: .value("Frequency", String(index)), y: .value("Magnitude", magnitud))
+                        
+                    }
+                    .onReceive(timer, perform: updateData)
+                    .chartYScale(domain: -3 ... Constants.magLimit)
+                    .chartXAxis(.hidden)
+                    .background(Color.secondary.opacity(0.2))
+                    .cornerRadius(10)
+                }
+            }
+            .font(.title2)
+            .padding()
+//            .background(Color.secondary.opacity(0.2))
+            .cornerRadius(10)
+            .padding(.horizontal)
         }
-        .padding()
-    }
+            .onAppear(perform: onAppear)
+            .onDisappear(perform: onDisappear)
+        }
+    
+    // Start accelerometer updates when the view appears
+        func onAppear() {
+            accelerometerManager.startUpdates()
+        }
+        
+        // Stop accelerometer updates when the view disappears
+        func onDisappear() {
+            accelerometerManager.stopUpdates()
+        }
+        func updateData(_: Date) {
+            dataDict = accelerometerManager.arr
+            
+            x = dataDict.x.map {
+                min($0, Constants.magLimit)
+    
+            }
+            y = dataDict.y.map {
+                min($0, Constants.magLimit)
+    
+            }
+            z = dataDict.z.map {
+                min($0, Constants.magLimit)
+    
+            }
+            rx = dataDict.rx.map {
+                min($0, Constants.magLimit)
+    
+            }
+            ry = dataDict.ry.map {
+                min($0, Constants.magLimit)
+    
+            }
+            rz = dataDict.rz.map {
+                min($0, Constants.magLimit)
+    
+            }
+            
+//          print(data)
+        
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Sensors/SensorFeeder.swift
@@ -0,0 +1,101 @@
+//
+//  SensorFeeder.swift
+//  Sensors
+//
+//  Created by Gabrio Barbieri on 5/10/23.
+//
+
+import Foundation
+import CoreMotion
+import Combine
+
+struct SensorData {
+    var x: [Float]
+    var y: [Float]
+    var z: [Float]
+    var rx: [Float]
+    var ry: [Float]
+    var rz: [Float]
+}
+
+class AccelerometerManager : ObservableObject {
+    
+    private let motionManager = CMMotionManager()
+    @Published var x = 0.0
+    @Published var y = 0.0
+    @Published var z = 0.0
+    @Published var rx = 0.0
+    @Published var ry = 0.0
+    @Published var rz = 0.0
+    @Published var arr: SensorData = SensorData.init(x: [], y: [], z: [], rx: [], ry: [], rz: [])
+    
+    func startUpdates() {
+        guard motionManager.isAccelerometerAvailable else {
+            print("Accelerometer is not available.")
+            return
+        }
+        
+        motionManager.accelerometerUpdateInterval = 0.1 // Update interval in seconds
+        
+        motionManager.startAccelerometerUpdates(to: .main) { (data, error) in
+            if let error = error {
+                print("Error receiving accelerometer data: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let accelerometerData = data else {
+                print("No accelerometer data available.")
+                return
+            }
+            
+            // Access accelerometer data
+            let acceleration = accelerometerData.acceleration
+            self.x = acceleration.x
+            self.y = acceleration.y
+            self.z = acceleration.z
+            
+            // Process the accelerometer data or perform any desired actions
+            print("Accelerometer data: x=\( self.x), y=\( self.y), z=\( self.z)")
+            
+            self.arr.x.append(Float(self.x))
+            self.arr.y.append(Float(self.y))
+            self.arr.z.append(Float(self.z))
+
+//            print(self.xa)
+        }
+        guard motionManager.isGyroAvailable else {
+                    print("Gyroscope is not available.")
+                    return
+                }
+                
+                motionManager.gyroUpdateInterval = 0.1 // Update interval in seconds
+                
+                motionManager.startGyroUpdates(to: .main) { (data, error) in
+                    if let error = error {
+                        print("Error receiving gyroscope data: \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    guard let gyroData = data else {
+                        print("No gyroscope data available.")
+                        return
+                    }
+                    
+                    // Access gyroscope data
+                    let rotationRate = gyroData.rotationRate
+                    self.rx = rotationRate.x
+                    self.ry = rotationRate.y
+                    self.rz = rotationRate.z
+                    self.arr.rx.append(Float(self.x))
+                    self.arr.ry.append(Float(self.y))
+                    self.arr.rz.append(Float(self.z))
+                    // Process the gyroscope data or perform any desired actions
+                    print("Gyroscope data: x=\( self.x), y=\( self.y), z=\( self.z)")
+                }
+    }
+    
+    func stopUpdates() {
+        motionManager.stopAccelerometerUpdates()
+        motionManager.stopGyroUpdates()
+    }
+}
